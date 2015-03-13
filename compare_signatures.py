170e0918d0495fed9e9f07954b3770ef48ed3b94
Gabriel Birke <gb@birke-software.de>
2015-03-13 17:58:01 +0100

    Store comparison results in sqlite db

--- compare_signatures.py
+++ compare_signatures.py
@@ -1,11 +1,14 @@
 import glob
 import pickle
 import os.path
+import sys
+import sqlite3
 
 import minhash
 import config
 
 result_data = {}
+sql_data = []
 
 if len(sys.argv) < 2:
     data_dir = "test_data"
@@ -14,6 +17,7 @@ else:
 
 for change_rate in config.CHANGE_RATES:
     result_data[change_rate] = {}
+    actual_similarity = 1 - 1/float(100/change_rate)
     for shingle_type in config.SHINGLE_CONFIG:
         result_data[change_rate][shingle_type] = {}
         for size in config.SHINGLE_CONFIG[shingle_type]:
@@ -24,6 +28,19 @@ for change_rate in config.CHANGE_RATES:
                 b = pickle.load(b_data)
                 similarity = minhash.minhash_signatures(a["hashes"], b["hashes"])
                 result_data[change_rate][shingle_type][size] = "{:.2f}".format(similarity * 100)
+                sql_data.append([data_dir, actual_similarity, shingle_type, size, similarity])
 
 for change_rate in result_data:
     print change_rate, result_data[change_rate]
+
+# export result data to a db for easier querying
+import sqlite3
+conn = sqlite3.connect('comparison_results.db')
+
+c = conn.cursor()
+c.execute('''CREATE TABLE IF NOT EXISTS comparison_results
+             (data_dir text, actual_similarity int, shingle_type text, shingle_size int, similarity_rating real)''')
+c.execute('DELETE FROM comparison_results WHERE data_dir = :dd ', {"dd": data_dir})
+for data in sql_data:
+    c.execute("INSERT INTO comparison_results VALUES (?,?,?,?,?)", data)
+conn.commit()
