4ea36e4c62858d1afb548e5d0f97081039ce0a71
Gabriel Birke <gb@birke-software.de>
2015-03-15 22:32:56 +0100

    Set default configuration

--- config.py
+++ config.py
@@ -1,9 +1,13 @@
-CHANGE_RATES = [1, 5, 10, 25, 50]
+#CHANGE_RATES = [1,3,5,8] + range(10,55,5)
+#CHANGE_RATES = [1,5,10,25,50]
+#CHANGE_RATES = range(1,11) + range(10,55,5)
+CHANGE_RATES = range(1,25) + range(25,55,5)
 
 SHINGLE_CONFIG = {
-    "byte": [6, 8, 12],
-    "word": [3, 5, 8],
-    "triple": [1, 2, 3, 5]
+#    "byte": [2, 4, 8, 16],
+    "word":   [2, 3, 5],
+    "triple": [2, 3, 5],
+    "turtle": [2, 3, 5]
 }
 
-NUM_HASHES = 200
+NUM_HASHES = 500
