c5141ae1b0e1117fb515d914650c744e752362e0
Gabriel Birke <gb@birke-software.de>
2015-03-16 19:54:53 +0100

    New config
    With our improved accuracy we don't need the diffent sampling sizes any
    longer.

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 #CHANGE_RATES = [1,3,5,8] + range(10,55,5)
 #CHANGE_RATES = [1,5,10,25,50]
 #CHANGE_RATES = range(1,11) + range(10,55,5)
-CHANGE_RATES = range(1,35) + range(35,55,5)
+CHANGE_RATES = range(1,55,5)
 
 SHINGLE_CONFIG = {
 #    "byte": [2, 4, 8, 16],
